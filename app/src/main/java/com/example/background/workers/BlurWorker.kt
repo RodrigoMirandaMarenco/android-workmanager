5fa150456937c16e663da9e3e98f1d42c9953cdf
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2022-01-04 12:26:11 -0600

    ✨ Use input data in BlurWorker

--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -2,9 +2,13 @@ package com.example.background.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.text.TextUtils
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import com.example.background.KEY_IMAGE_URI
 import com.example.background.R
 
 /**
@@ -14,15 +18,28 @@ class BlurWorker(context: Context, params: WorkerParameters) : Worker(context, p
 
     override fun doWork(): Result {
         val appContext = applicationContext
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
         makeStatusNotification(appContext.getString(R.string.blurring_image), appContext)
 
         return try {
+            if (TextUtils.isEmpty(resourceUri)) {
+                Log.e(TAG, "Empty resourceUri")
+                throw IllegalArgumentException("Empty resourceUri")
+            }
+
             val picture =
-                BitmapFactory.decodeResource(appContext.resources, R.drawable.android_cupcake)
+                BitmapFactory.decodeStream(
+                    appContext.contentResolver.openInputStream(
+                        Uri.parse(
+                            resourceUri
+                        )
+                    )
+                )
             val blurredPicture = blurBitmap(picture, appContext)
-            val uri = writeBitmapToFile(appContext, blurredPicture)
-            makeStatusNotification(appContext.getString(R.string.output_is, uri), appContext)
-            Result.success()
+            val outputUri = writeBitmapToFile(appContext, blurredPicture)
+            makeStatusNotification(appContext.getString(R.string.output_is, outputUri), appContext)
+
+            Result.success(workDataOf(KEY_IMAGE_URI to outputUri.toString()))
         } catch (e: Throwable) {
             Log.e(TAG, "Error applying blur")
             Result.failure()
