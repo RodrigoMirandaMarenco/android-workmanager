d3f99a4bb6c97b48009397fa62c9e847264776ec
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2022-01-05 17:02:01 -0600

    ✅ Add testAppliesBlur

--- app/src/androidTest/java/com/example/background/workers/BlurWorkerTest.kt
+++ app/src/androidTest/java/com/example/background/workers/BlurWorkerTest.kt
@@ -4,6 +4,9 @@ import WorkManagerTestRule
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo.State.FAILED
+import androidx.work.WorkInfo.State.SUCCEEDED
+import androidx.work.workDataOf
+import com.example.background.KEY_IMAGE_URI
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.assertThat
 import org.junit.Rule
@@ -28,4 +31,22 @@ class BlurWorkerTest {
         assertThat(info.state, `is`(FAILED))
     }
 
+    @Test
+    @Throws(Exception::class)
+    fun testAppliesBlur() {
+        val inputDataUri = copyFileFromTestToTargetCtx(
+            workManagerRule.testContext,
+            workManagerRule.targetContext,
+            "test_image.png"
+        )
+        val inputData = workDataOf(KEY_IMAGE_URI to inputDataUri.toString())
+
+        val request = OneTimeWorkRequestBuilder<BlurWorker>().setInputData(inputData).build()
+        workManagerRule.workManager.enqueue(request).result.get()
+        val info = workManagerRule.workManager.getWorkInfoById(request.id).get()
+        val outputUri = info.outputData.getString(KEY_IMAGE_URI)
+
+        assertThat(uriFileExists(workManagerRule.targetContext, outputUri), `is`(true))
+        assertThat(info.state, `is`(SUCCEEDED))
+    }
 }
